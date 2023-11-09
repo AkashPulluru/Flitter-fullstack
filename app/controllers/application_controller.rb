bf14b2c144d4bfdcec0065747595b6e80dd3d9d7
Akash Pulluru <akashpulluru@gmail.com>
2023-11-09 17:09:22 -0500

    still working on migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,24 @@ def snake_case_params
   params.deep_transform_keys!(&:underscore)
 end
 
+def current_user
+    @current_user ||= # user whose `session_token` == token in `session` cookie
+  end
+  
+  def login!(user)
+    # reset `user`'s `session_token` and store in `session` cookie
+  end
+  
+  def logout!
+    # reset the `current_user`'s session cookie, if one exists
+    # clear out token from `session` cookie
+    @current_user = nil # so that subsequent calls to `current_user` return nil
+  end
+  
+  def require_logged_in
+    unless current_user
+      render json: { message: 'Unauthorized' }, status: :unauthorized 
+    end
+  end
+  
 end
