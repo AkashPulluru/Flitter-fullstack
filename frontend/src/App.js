e57c5da5d9599634de76a8e40c85361023cc13b7
Akash Pulluru <akashpulluru@gmail.com>
2023-11-28 08:39:01 -0500

    login redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
-// App.js
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom'; 
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
 import Navigation from './components/Navigation'; 
 import PhotosComponent from './components/PhotosComponent';
 import UserShowPage from './components/UserShowPage';
 import PhotoDetailPage from './components/PhotoDetailPage';
-// import Form from './Form';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
@@ -27,13 +25,13 @@ function App() {
         <Route path="/login">
           <LoginFormPage />
         </Route>
+        <Route exact path="/">
+          <Redirect to="/login" /> {/* Redirect from home to login */}
+        </Route>
         <Route path="/signup">
           <SignupFormPage />
         </Route>
         <Route path="/users/:id" component={UserShowPage} />
-        {/* <Route path="/photos">
-          <PhotosComponent />
-        </Route> */}
         <Route path="/photos/:id" component={PhotoDetailPage} />
       </Switch>
       {/* <Form setNewPost={setNewPost} /> */}
